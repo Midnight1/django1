a5f3e90a3b5c18b5339296e33a590e118ff0aa9a
Jared <hellghost_1@hotmail.com>
2015-09-17 08:43:07 -0600

    first time

---
+++ main_site/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
